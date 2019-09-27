d4a4d95f556e2ecfdbb86c30c7779955e1b9d89d
Kiffin Gish <kiffin.gish@planet.nl>
2019-09-27 16:35:50 +0200

    Task create service.

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,11 +1,25 @@
 import { Injectable } from '@nestjs/common';
 import { Task } from './task.model';
 
+import * as uuid from 'uuid/v4';
+
 @Injectable()
 export class TasksService {
   private tasks: Task[] = [];
 
   getAllTasks(): Task[] {
     return this.tasks;
+  }
+
+  createTask(title: string, description: string): Task {
+    const task: Task = {
+      id: uuid(),
+      title,
+      description,
+      status: 'open',
+    };
+
+    this.tasks.push(task);
+    return task;
   }
 }
