88f05b3f85cbbb0c674ff1e1df5ef46d27d8c17c
Kiffin Gish <kiffin.gish@planet.nl>
2019-09-27 21:42:10 +0200

    Task create controller.

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 
 import { TasksService } from './tasks.service';
 import { Task } from './task.model';
@@ -12,5 +12,13 @@ export class TasksController {
   @Get()
   getAllTasks(): Task[] {
     return this.tasksService.getAllTasks();
+  }
+
+  @Post()
+  createTask(
+    @Body('title') title,
+    @Body('description') description,
+  ): Task {
+    return this.tasksService.createTask(title, description);
   }
 }
