e4295e2158be0bbafdeb20b03189f3eb5203df58
Kiffin Gish <kiffin.gish@planet.nl>
2019-09-28 19:21:18 +0200

    Delete task by id service and controller.

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
 
 import { TasksService } from './tasks.service';
 import { Task } from './task.model';
@@ -23,5 +23,10 @@ export class TasksController {
   @Post()
   createTask(@Body() createTaskDto: CreateTaskDto): Task {
     return this.tasksService.createTask(createTaskDto);
+  }
+
+  @Delete('/:id')
+  deleteTask(@Param('id') id: string): void {
+    this.tasksService.deleteTask(id);
   }
 }
