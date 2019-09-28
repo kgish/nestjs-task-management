ba409073cf020fcc662b226c35ee4db86519d574
Kiffin Gish <kiffin.gish@planet.nl>
2019-09-28 16:58:15 +0200

    Get task by id service and controller.

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -12,8 +12,13 @@ export class TasksService {
     return this.tasks;
   }
 
+  getTaskById(id: string): Task {
+    return this.tasks.find(task => task.id === id);
+  }
+
   createTask(createTaskDto: CreateTaskDto): Task {
     const { title, description } = createTaskDto;
+
     const task: Task = {
       id: uuid(),
       title,
