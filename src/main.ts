f0ef664e9800eff9f49ba086316c077909916d6a
Kiffin Gish <kiffin.gish@planet.nl>
2019-10-09 22:13:19 +0200

    Setting up and using config.

--- src/main.ts
+++ src/main.ts
@@ -6,11 +6,12 @@ import { AppModule } from './app.module';
 import * as config from 'config';
 
 async function bootstrap() {
-  const serverConfig = config.get('server');
   const logger = new Logger('bootstrap');
   const app = await NestFactory.create(AppModule);
 
-  const port = process.env.PORT || 3000;
+  const serverConfig = config.get('server');
+
+  const port = process.env.PORT || serverConfig.port;
   await app.listen(port);
 
   logger.log(`Application listening on port ${port}`);
